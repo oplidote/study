685723ceae5a1d868013a2bedbb769ba16546359
jhwmac <jkr2505@naver.com>
2022-01-12 11:21:08 +0900

    folder add stxconst start

--- wire_7/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-    let sub_menu = $('.gnb > li');
-    sub_menu.mouseenter(function(){
-        $(this).find('.submenu').stop.slideDown(300);
-    });
-    sub_menu.mouseleave(function(){
-        $(this).find('.submenu').stop.slideUp(300);
-    });
-});
