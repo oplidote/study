e440566aaf27f4db244b81bf4960fd9bb6efac89
jhwmac <jkr2505@naver.com>
2022-02-17 14:30:35 +0900

    jquery folder

---
+++ jquery/js/script.js
@@ -0,0 +1,17 @@
+// 최초 기본형
+// html,css,js 만 로딩 완료 감시..
+$(document).ready(function () {
+    $('.gnb').css('border','5px solid red');
+    $('.gnb li').css('border','10px solid green');
+    $('ul[class=gnb]').css('background', 'pink');
+
+});
+
+
+
+
+
+// html,css,js,image,sound,movie..;
+window.onload = function(){
+
+}
